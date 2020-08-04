9584dd8c6965737ed22403a8438efccc92742cec
Tirdad <abbasi.tirdad@gmail.com>
2020-08-04 17:57:08 +0430

    add color setting to homepage banners

--- src/Entities/Banner.php
+++ src/Entities/Banner.php
@@ -8,18 +8,24 @@ use Tir\Setting\Facades\Stg;
 class Banner
 {
     public $caption_1;
+    public $caption_1_color;
     public $caption_2;
+    public $caption_2_color;
     public $call_to_action_text;
+    public $call_to_action_color;
     public $call_to_action_url;
     public $open_in_new_window;
     public $image;
 
-    public function __construct($caption_1, $caption_2, $call_to_action_text, $call_to_action_url, $open_in_new_window, $image)
+    public function __construct($caption_1,$caption_1_color, $caption_2, $caption_2_color, $call_to_action_text, $call_to_action_color, $call_to_action_url, $open_in_new_window, $image)
     {
         $this->caption_1 = $caption_1;
+        $this->caption_1_color = $caption_1_color;
         $this->caption_2 = $caption_2;
+        $this->caption_2_color = $caption_2_color;
         $this->call_to_action_text = $call_to_action_text;
         $this->call_to_action_url = $call_to_action_url;
+        $this->call_to_action_color = $call_to_action_color;
         $this->open_in_new_window = $open_in_new_window;
         $this->image = $image;
     }
@@ -53,8 +59,11 @@ class Banner
     {
         return new self(
             Stg::get("{$name}_caption_1"),
+            Stg::get("{$name}_caption_1_color"),
             Stg::get("{$name}_caption_2"),
+            Stg::get("{$name}_caption_2_color"),
             Stg::get("{$name}_call_to_action_text"),
+            Stg::get("{$name}_call_to_action_color"),
             Stg::get("{$name}_call_to_action_url"),
             Stg::get("{$name}_open_in_new_window"),
             Stg::get("{$name}_image")
